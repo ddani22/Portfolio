2d4ac689f6b331d2490d26937387d25d14402be1
ddani22 <danimoreno2602@gmail.com>
2025-11-06 12:07:44 +0100

    Agrego astro.config.mjs para desplegar github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ddani22.github.io', 
+  
+  base: '/Portfolio',
+  
   integrations: [tailwind()]
 });
